9240363a54fd84068c0b6eaff29f628a1580f95c
jeyalakshmik1 <jeyalakshmi.kunjumadan@outlook.com>
2025-01-26 19:56:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server    
+var express = require('express');
+var app = express();
+
+// Create a route for the comments
+app.get('/comments', function(req, res) {
+    res.send('This is the comments page');
+});
+
+// Start the server
+app.listen(3000, function() {
+    console.log('Server is running on http://localhost:3000');
+});
